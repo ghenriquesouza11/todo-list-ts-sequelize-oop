57204e6625f5d588f646c02f98942e6813d68c4f
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-08-29 18:01:26 -0300

    feat: dependencias do projeto e configurações básicas do sequelize e do typescript

---
+++ backend/build/database/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+const config = {
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
